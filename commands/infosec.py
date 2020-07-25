3d1ea12b666ea405d481855ebe2506f5aef14abb
Matt Watkins <mattwhatkins@gmail.com>
2020-07-25 12:44:04 +0100

    Adding domaintools and riskiq infosec commands

--- commands/infosec.py
+++ commands/infosec.py
@@ -31,3 +31,33 @@ class shodan(BunnyCommand):
             return f"{self.url}search?query={qp(args)}"
         else:
             return self.url
+
+
+class domaintools(BunnyCommand):
+    def __init__(self):
+        super().__init__()
+        self.aliases = ["whois", "domaintools", "dt"]
+        self.description = "Searches DomainTools"
+        self.name = "DomainTools"
+        self.url = "https://whois.domaintools.com/"
+
+    def run(self, args, request):
+        if args:
+            return f"{self.url}{qp(args)}"
+        else:
+            return self.url
+
+
+class riskiq(BunnyCommand):
+    def __init__(self):
+        super().__init__()
+        self.aliases = ["riq", "riskiq"]
+        self.description = "Searches RiskIQ"
+        self.name = "Searches RiskIQ"
+        self.url = "https://community.riskiq.com/"
+
+    def run(self, args, request):
+        if args:
+            return f"{self.url}search/{qp(args)}"
+        else:
+            return self.url
